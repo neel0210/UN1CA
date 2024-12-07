9e39192b7fcbfb65d48d0410d5f016d0ee9a0823
local-build <localbuild@noreply.local>
2024-12-07 05:08:05 -0500

    UN1CA: target: add support for a22x

---
+++ target/a22x/config.sh
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A22 5g (a22x)
+TARGET_NAME="Galaxy A22 5G"
+TARGET_CODENAME="a22x"
+TARGET_FIRMWARE="SM-A226B/INS/351518941234565"
+TARGET_EXTRA_FIRMWARES=()
+TARGET_API_LEVEL=33
+TARGET_PRODUCT_FIRST_API_LEVEL=20
+TARGET_VNDK_VERSION=31
+TARGET_SINGLE_SYSTEM_IMAGE="qssi"
+TARGET_OS_FILE_SYSTEM="ext4"
+TARGET_SUPER_PARTITION_SIZE=7053063680
+TARGET_SUPER_GROUP_SIZE=7048869376
+TARGET_HAS_SYSTEM_EXT=false
+
+# SEC Product Feature
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_DUAL_SPEAKER=false
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
+TARGET_AUTO_BRIGHTNESS_TYPE="3"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_mt6833_xx"
+TARGET_ESE_CHIP_VENDOR="NXP"
+TARGET_ESE_COS_NAME="JCOP4.0"
+TARGET_FP_SENSOR_CONFIG=false
+TARGET_HAS_HW_MDNIE=true
+TARGET_HAS_MASS_CAMERA_APP=true
+TARGET_HAS_QHD_DISPLAY=false
+TARGET_HFR_MODE="0"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60,90"
+TARGET_HFR_DEFAULT_REFRESH_RATE="90"
+TARGET_IS_ESIM_SUPPORTED=false
+TARGET_MDNIE_SUPPORT_HDR_EFFECT=false
+TARGET_MDNIE_SUPPORTED_MODES="57105"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="3"
+TARGET_MULTI_MIC_MANAGER_VERSION="07010"
+TARGET_SSRM_CONFIG_NAME="siop_a22x_mt6833"
+TARGET_SUPPORT_CUTOUT_PROTECTION=false
