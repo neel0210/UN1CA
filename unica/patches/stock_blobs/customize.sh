0cc3723ab338a5b127a0623619db649bb80b869e
Neel <neeljadhavforcoc@gmail.com>
2024-12-13 04:46:22 -0500

    Update customize.sh

--- unica/patches/stock_blobs/customize.sh
+++ unica/patches/stock_blobs/customize.sh
@@ -87,12 +87,18 @@ REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/cameradata"
 
 if [ -d "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/cameradata" ]; then
     cp -a --preserve=all "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/cameradata" "$WORK_DIR/system/system/cameradata"
+    
     while read -r i; do
-    FILE="$(echo -n "$i"| sed "s.$WORK_DIR/system/..")"
-    [ -d "$i" ] && echo "$FILE 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
-    [ -f "$i" ] && echo "$FILE 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
-    FILE="$(echo -n "$FILE" | sed 's/\./\\./g')"
-    echo "/$FILE u:object_r:system_file:s0" >> "$WORK_DIR/configs/file_context-system"
+        FILE="$(echo -n "$i" | sed "s.$WORK_DIR/system/..")"
+        
+        [ -d "$i" ] && echo "$FILE 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+        
+        [ -f "$i" ] && echo "$FILE 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+        
+        FILE="$(echo -n "$FILE" | sed 's/\./\\./g')"
+        
+        echo "/$FILE u:object_r:system_file:s0" >> "$WORK_DIR/configs/file_context-system"
+    done
 fi
 
 if [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/usr/share/alsa/alsa.conf" ]; then
